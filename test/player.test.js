a343fcc3a9d1ea834cb16388041c7d45d99ab23e
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-26 19:39:18 +0100

    Red: The dealer exists

--- test/player.test.js
+++ test/player.test.js
@@ -3,3 +3,7 @@ const player = require('../src/Player');
 test("The player class exists", () => {
     expect(player).toBeDefined();
 });
+
+test("The dealer class exists", () => {
+    expect(dealer).toBeDefined();
+});
