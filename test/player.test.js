29cf87bd4a3b1230d523f38c26e862bd54008202
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-27 09:44:40 +0100

    Red: The dealer class exists

--- test/player.test.js
+++ test/player.test.js
@@ -3,3 +3,7 @@ const Player = require('../src/Player');
 test("The player class exists", () => {
     expect(new Player()).toBeInstanceOf(Player);
 });
+
+test("The dealer class esists", () => {
+    expect(new Dealer()).toBeInstanceOf(Dealer);
+})
