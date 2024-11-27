93e1dcdd7f72442ccfa1d1a28d1454eb172e4ba7
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-27 19:25:41 +0100

    Green: Possible to check if a card has a suit type

--- test/Card.test.js
+++ test/Card.test.js
@@ -1,7 +1,25 @@
 const Card = require('../src/Card');
+const Suit_types = require("../src/Suit_types");
 
-test("The Card class has a type", () => {
+test("The Card class has a suit", () => {
     const card = new Card();
     card.setSuit('hearts');
     expect(card.getSuit()).toBe('hearts');
 });
+
+test("The Card class has a suit that should be from the suit_type", () => {
+    const card = new Card();
+    card.setSuit('hearts');
+    expect(isFromTheSuiteType(card.getSuit())).toBe(true);
+    card.setSuit('Bubble');
+    expect(isFromTheSuiteType(card.getSuit())).toBe(false);
+});
+
+function isFromTheSuiteType(suit_given) {
+    for (const element in Suit_types) {
+        if (suit_given === Suit_types[element]) {
+            return true;
+        }
+    }
+    return false;
+}
--- test/Personliga.js
+++ test/Personliga.js
@@ -3,3 +3,14 @@ const Suit_types = require("../src/Suit_types");
 for (const element in Suit_types) {
     console.log(Suit_types[element]);
 }
+
+function isFromTheSuiteType(suit_given) {
+    for (const element in Suit_types) {
+        if (suit_given === Suit_types[element]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(isFromTheSuiteType('hearts'));
