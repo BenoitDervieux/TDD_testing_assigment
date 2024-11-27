d230d967ab865235a24e1f662e1ecdd03db6db6d
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-27 19:01:17 +0100

    Green: Cards have a Suit

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,17 @@
 class Card {
+    #suit = '';
+    #rank = '';
     constructor() {
     }
+
+    getSuit() {
+        return this.#suit;
+    }
+    setSuit(suit) {
+        this.#suit = suit;
+    }
+
+
 }
 
   module.exports = Card;
--- test/Card.test.js
+++ test/Card.test.js
@@ -2,6 +2,6 @@ const Card = require('../src/Card');
 
 test("The Card class has a type", () => {
     const card = new Card();
-    card.setType('hearts');
-    expect(card.getType()).toBe('hearts');
+    card.setSuit('hearts');
+    expect(card.getSuit()).toBe('hearts');
 });
