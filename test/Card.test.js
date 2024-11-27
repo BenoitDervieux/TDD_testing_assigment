66184178673c3986276919330f94b1c05b346e84
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-27 20:00:29 +0100

    Refactoring: The correct suit type

--- test/Card.test.js
+++ test/Card.test.js
@@ -2,15 +2,10 @@ const Card = require('../src/Card');
 const Suit_types = require("../src/Suit_types");
 const WrongInputException = require('../src/WrongInputException')
 
-test("The Card class has a suit", () => {
+test("The Card class has a suit from the suit_type", () => {
     const card = new Card();
     card.setSuit('hearts');
     expect(card.getSuit()).toBe('hearts');
-});
-
-test("The Card class has a suit that should be from the suit_type", () => {
-    const card = new Card();
     expect(() => card.setSuit('Bubble')).toThrow("Invalid input type");
-    // expect(card.setSuit('Bubble')).toBeInstanceOf(WrongInputException);
 });
 
