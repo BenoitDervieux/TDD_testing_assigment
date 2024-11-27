80c9051a7169cc6f42a68529d130d22c717844f7
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-27 09:43:36 +0100

    refactor: class creation inside test

--- test/player.test.js
+++ test/player.test.js
@@ -1,6 +1,5 @@
 const Player = require('../src/Player');
 
 test("The player class exists", () => {
-    let player = new Player();
-    expect(player).toBeInstanceOf(Player);
+    expect(new Player()).toBeInstanceOf(Player);
 });
