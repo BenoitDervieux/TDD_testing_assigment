92deb37290362bfcfe81852032c8206c35c32193
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-28 17:25:13 +0100

    Red: It is possible to draw a card from the deck

--- test/Deck.test.js
+++ test/Deck.test.js
@@ -1,7 +1,14 @@
 const Deck = require('../src/Deck');
+const Card = require('../src/Card');
 
 
 test("The deck has 52 Cards", () => {
     const deck = new Deck();
     expect(deck.getNumberOfCards()).toBe(52);
 } )
+
+test("It is possible to draw card from the deck", () => {
+    const deck = new Deck();
+    const card = deck.draw_card();
+    expect(card).toBeInstanceOf(Card);
+})
