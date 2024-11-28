09caba72b1f354440b3dba326ec3e92f7230077d
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-28 21:59:23 +0100

    Green: Class hand has cards attribute

--- src/Hand.js
+++ src/Hand.js
@@ -1,5 +1,10 @@
 class Hand {
+    #cards = [];
     constructor() {
+    }
+
+    getCards() {
+        return (this.#cards.length == 0) ? null : this.#cards;
     }
 }
 
--- test/Hand.test.js
+++ test/Hand.test.js
@@ -2,5 +2,5 @@ const Hand = require('../src/Hand');
 
 test("The hand class has cards", () => {
     const hand = new Hand();
-    expect(hand.getNumberOfCards()).toBe(0);
+    expect(hand.getCards()).toBe(null);
 })
