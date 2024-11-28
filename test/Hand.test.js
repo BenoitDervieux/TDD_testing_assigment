184451c46f6ddf193b1200bfb12e5331e7fe4854
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-28 22:10:27 +0100

    refactoring: Hand tests

--- test/Hand.test.js
+++ test/Hand.test.js
@@ -1,15 +1,12 @@
 const Hand = require('../src/Hand');
 const Card = require('../src/Card');
 
-test("The hand class has cards", () => {
+test("The hand class can add cards and get them", () => {
     const hand = new Hand();
     expect(hand.getCards()).toBe(null);
-})
-
-test("The hand class can add cards", () => {
-    const hand = new Hand();
     const card = new Card();
     card.setRank('Ace');
     card.setSuit('hearts');
     expect(hand.addCard(card)).toBe(true);
+    expect(hand.getCards().length).toBe(1);
 })
