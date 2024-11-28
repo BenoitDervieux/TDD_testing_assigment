29afdf6a12e5c05404d53723df8bf40486a396e2
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-28 22:04:18 +0100

    Red: The hand class has no method add

--- test/Hand.test.js
+++ test/Hand.test.js
@@ -1,6 +1,15 @@
 const Hand = require('../src/Hand');
+const Card = require('../src/Card');
 
 test("The hand class has cards", () => {
     const hand = new Hand();
     expect(hand.getCards()).toBe(null);
+})
+
+test("The hand class can add cards", () => {
+    const hand = new Hand();
+    const card = new Card();
+    card.setRank('Ace');
+    card.setSuit('hearts');
+    expect(hand.add(card)).toBe(true);
 })
