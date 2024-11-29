7c0b95c26b3d8a8a252dcb24a607af91008e16f9
Benoit Dervieux <bd222ch@student.lnu.se>
2024-11-29 20:06:56 +0100

    Red: The hand cannot handle an ace card with other values

--- test/Hand.test.js
+++ test/Hand.test.js
@@ -42,12 +42,25 @@ test("the hand class can calculate the value of 1 cards with no ace", () => {
 test("the hand class can calculate the value of 2 cards with no ace", () => {
     const hand = new Hand();
     const card1 = new Card();
-    card1.setRank('Two');
+    card1.setRank('Queen');
     card1.setSuit('hearts');
     hand.addCard(card1)
     const card2 = new Card();
-    card2.setRank('Three');
+    card2.setRank('King');
     card2.setSuit('hearts');
     hand.addCard(card2)
-    expect(hand.getValue()).toBe(5);
+    expect(hand.getValue()).toBe(22);
+})
+
+test("the hand class can calculate the value of 2 cards with an ace and a card lower than 11", () => {
+    const hand = new Hand();
+    const card1 = new Card();
+    card1.setRank('ten');
+    card1.setSuit('hearts');
+    hand.addCard(card1)
+    const card2 = new Card();
+    card2.setRank('Ace');
+    card2.setSuit('hearts');
+    hand.addCard(card2)
+    expect(hand.getValue()).toBe(21);
 })
