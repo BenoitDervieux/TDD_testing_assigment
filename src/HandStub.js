055210d52c0a68634801c63b3bb17e7bbf01b955
Benoit Dervieux <bd222ch@student.lnu.se>
2024-12-01 15:18:18 +0100

    Red: The player has a hand

---
+++ src/HandStub.js
@@ -0,0 +1,9 @@
+const Hand = require('../src/Hand');
+
+class HandStub extends Hand {
+    constructor() {
+        super();        
+    }
+}
+
+module.exports = HandStub;
--- test/Player.test.js
+++ test/Player.test.js
@@ -1,4 +1,6 @@
 const Player = require('../src/Player');
+const HandStub = require('../src/HandStub');
+const Hand = require('../src/Hand');
 
 
 test("The player has a name and a budget", () => {
@@ -6,4 +8,10 @@ test("The player has a name and a budget", () => {
     player.setName("Player1");
     expect(player.getName()).toBe("Player1");
     expect(player.getBudget()).toBe(100);
+})
+
+
+test("The player has a hand", () => {
+    const player = new Player();
+    expect(player.getHand()).toBeInstanceOf(Hand);
 })
