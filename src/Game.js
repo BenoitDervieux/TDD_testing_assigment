949cb6358731ab74edb5a1a61682fb5f8bfadc77
Benoit Dervieux <bd222ch@student.lnu.se>
2024-12-02 09:52:59 +0100

    Green: The game class has a dealer

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,13 @@
+const Dealer = require('../src/Dealer');
+
 class Game {
+  #dealer;
     constructor() {
+      this.#dealer = new Dealer();
+    }
+
+    getDealer() {
+      return this.#dealer;
     }
 }
 
